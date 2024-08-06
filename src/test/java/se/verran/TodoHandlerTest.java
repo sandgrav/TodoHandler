cb2e7b3271d22f27ceccc3dd7f56635a440c63b3
Morten Sandgrav <morten@sandgrav.se>
2024-08-06 12:34:17 +0200

    Initial commit

---
+++ src/test/java/se/verran/TodoHandlerTest.java
@@ -0,0 +1,37 @@
+package se.verran;
+
+import static org.junit.jupiter.api.Assertions.*;
+class TodoHandlerTest {
+
+    @org.junit.jupiter.api.Test
+    void saveNote() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void checkForSubjectConflict() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void checkForInvalidContentLength() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void verifyAvailableMemory() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void checkForInvalidSubjectLength() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getNotes() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getNoteSubjectLengthLimit() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getNoteContentLengthLimit() {
+    }
+}
